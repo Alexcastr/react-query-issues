cc9827f43eced6b777044834cc321a885734ab70
alexander <castro.t.alex@gmail.com>
2022-12-03 12:12:46 -0500

    variables already fixed

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers:{
-    Authorization: `Bearer ${import.meta.env.local.VITE_GITHUB_TOKEN}`
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
     
   }
 })
